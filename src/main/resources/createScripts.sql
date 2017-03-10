25bab6d07310776543a49322f56d644f3510179d
kamendoza <v-kamendoza@ean.com>
2017-03-10 10:41:12 -0600

    MASTER - update the db script for creatin the table

--- src/main/resources/createScripts.sql
+++ src/main/resources/createScripts.sql
@@ -1,7 +1,30 @@
-CREATE TABLE `news` (
+CREATE TABLE `order_product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `author` varchar(50) NOT NULL,
-  `news` varchar(400) NOT NULL,
+  `id_product` int(11) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `id_order` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+  KEY `FK_orders_idx` (`id_product`),
+  KEY `fk_order_product_product_idx` (`id_product`),
+  KEY `fk_orders_product_orders_idx` (`id_order`),
+  CONSTRAINT `fk_orders_product_orders` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orders_product_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `order_date` date NOT NULL,
+  `user_name` varchar(45) NOT NULL,
+  `total` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `inventory` int(11) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
